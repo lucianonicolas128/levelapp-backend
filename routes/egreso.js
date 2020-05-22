2601ec0bebe47f0dc499278e4e37f45e9d0ca8b0
lucianonicolas128 <luciano.prina@hotmail.es>
2020-05-22 08:32:20 -0300

    actualizacion de error

--- routes/egreso.js
+++ routes/egreso.js
@@ -6,10 +6,10 @@ var routerEgreso = express.Router();
 var crypto = require('crypto')
 var multer = require('multer');
 
-router.post('/save-egreso', EgresoController.saveegreso);
-router.get('/egreso/:id?', EgresoController.getegreso);
-router.get('/egresos', EgresoController.getegresos);
-router.put('/egreso/:id', EgresoController.updateegreso);
-router.delete('/egreso/:id', EgresoController.deleteegreso);
+router.post('/save-egreso', EgresoController.saveEgreso);
+router.get('/egreso/:id?', EgresoController.getEgreso);
+router.get('/egresos', EgresoController.getEgresos);
+router.put('/egreso/:id', EgresoController.updateEgreso);
+router.delete('/egreso/:id', EgresoController.deleteEgreso);
 
 module.exports = router;
